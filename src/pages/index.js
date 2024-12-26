17ae690e453a434fc379e46664f0699d2a1975e1
Colin Chiang <colinchiang.cc@gmail.com>
2024-12-26 18:28:50 +0800

    Fix margin for title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ const initialTasks = [
 
 export default function Index() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="my-4 mt-20">
+    <div className="flex flex-col items-center justify-center my-20">
+      <div className="mb-4 mx-8">
         <h1 className="text-4xl my-2 font-semibold">todo-app</h1>
         <p className="mb-4 text-xl"><i>a simple todo app created with React and Next.js.</i></p>
       </div>
