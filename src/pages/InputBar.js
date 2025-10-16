20d1ea06e7d2eb9b2a210ed2ee535ef9add411ce
Colin Chiang <colinchiang.cc@gmail.com>
2025-10-16 21:19:39 +0800

    Replace add button with icon

--- src/pages/InputBar.js
+++ src/pages/InputBar.js
@@ -1,25 +1,35 @@
 import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
-export default function InputBar({onAddTask}) {
+export default function InputBar({ onAddTask }) {
+  const [text, setText] = useState('');
 
-    const [text, setText] = useState('');
-
-    function handleAddTask() {
-        setText('');
-        onAddTask(text);
-    }
-    return (
-      <div id="task-input-bar">
-        <input 
-          id="task-input-bar-field" 
-          type="text" 
-          placeholder="Enter a task" 
-          value={text} onChange={e => setText(e.target.value)}
-          />
-        { (text == "")
-          ? <button className="button disabled-button" id="add-button" disabled>Add</button>
-          : <button className="button button-success" id="add-button" onClick={handleAddTask}>Add</button>
-        }
-      </div>
-    )
+  function handleAddTask() {
+    setText('');
+    onAddTask(text);
   }
+
+  return (
+    <div id="task-input-bar">
+      <input
+        id="task-input-bar-field"
+        type="text"
+        placeholder="Enter a task"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      {(text === "")
+        ? (
+          <button className="button disabled-button" id="add-button" disabled>
+            <PlusIcon className="h-5 w-5" />
+          </button>
+        )
+        : (
+          <button className="button button-success" id="add-button" onClick={handleAddTask}>
+            <PlusIcon className="h-5 w-5" />
+          </button>
+        )
+      }
+    </div>
+  );
+}
